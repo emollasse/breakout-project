a738e27ce404a1d57566b848990d924de3cf1689
Nicolas Wilhelm <nicolas.wi@free.fr>
2016-05-19 15:57:13 +0200

    Creation of the ball and creation of collision
    Creation of the class Ball.
    Collision is a method of the class Ball and can test the collision between
    the ball and any paddle or brick of the game.
    
    But there is still bug about collision :
    -The ball can be in the paddle when the paddle move in same time as the
    collision with the ball.
    -The rebound in the coin is sometimes not logical.

--- Barre.py
+++ Barre.py
@@ -6,7 +6,9 @@ class Barre:
         self.image = pygame.image.load(image).convert()
         self.x = 325
         self.y = 550
-        self.vitesse = 5
+        self.vitesse = 1
+        self.sizew = 150
+        self.sizeh = 19
 
     def get_image(self):
         return self.image
@@ -20,3 +22,49 @@ class Barre:
             self.x += self.vitesse
             if self.x > 650:
                 self.x = 650
+
+
+class Ball:
+    def __init__(self, image):
+        self.image = pygame.image.load(image).convert_alpha()
+        self.rayon = 12.5
+        self.x = 400-self.rayon
+        self.y = 550-2*self.rayon
+        self.vitessex = 1
+        self.vitessey = -1
+        self.start_position = True
+
+    def movement(self, paddle):
+        if self.start_position == True:
+            self.x = paddle.x + 150/2 - self.rayon
+            self.y = paddle.y- 2*self.rayon
+        else :
+            self.x += self.vitessex
+            self.y += self.vitessey
+            self.collision(paddle)
+            if self.x < 0 or self.x > 800-2*self.rayon:
+                if self.x < 0 : self.x = 0
+                elif self.x > 800-2*self.rayon : self.x = 800-2*self.rayon
+                self.vitessex *=-1
+            if self.y < 0 or self.y > 650-2*self.rayon:
+                if self.y < 0 : self.y = 0
+                elif self.y > 650-2*self.rayon: self.y = 650-2*self.rayon
+                self.vitessey *=-1
+
+    def go(self):
+        if self.start_position == True:
+            self.start_position = False
+        else :
+            self.start_position = True
+
+    def collision(self, paddle):
+        if (self.y + 2*self.rayon > paddle.y and self.y < paddle.y + paddle.sizeh) and (self.x + 2*self.rayon > paddle.x and self.x < paddle.x + paddle.sizew):
+            if (self.x + self.rayon > paddle.x and self.x + self.rayon < paddle.x + paddle.sizew) and (self.y + self.rayon < paddle.y or self.y + self.rayon > paddle.y+paddle.sizeh):
+                self.vitessey *=-1
+            elif (self.y + self.rayon > paddle.y and self.y + self.rayon < paddle.y + paddle.sizeh) and (self.x + self.rayon < paddle.x or self.x + self.rayon > paddle.x+paddle.sizew):
+                self.vitessex *=-1
+            else :
+                self.vitessex *=-1
+                self.vitessey *=-1
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from Barre import *
+import time
 
 pygame.init()
 
@@ -9,23 +10,32 @@ fenetre = pygame.display.set_mode((800, 650))
 continuer = True
 
 barre = Barre("Img/Barre.png")
+ball = Ball("Img/Ball.png")
 
-pygame.key.set_repeat(10,10)
+t=time.time()
 
 while continuer:
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = False
         elif event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                barre.movement("left")
-            elif event.key == K_RIGHT:
-                barre.movement("right")
-            elif event.key == K_ESCAPE:
+            if event.key == K_ESCAPE:
                 continuer = False
+            elif event.key == K_SPACE:
+                ball.go()
+
+    if time.time() - t > 0.001:
+        tkey = pygame.key.get_pressed()
+        if tkey[K_LEFT] != 0:
+            barre.movement("left")
+        elif tkey[K_RIGHT] != 0:
+            barre.movement("right")
+        ball.movement(barre)
+        t = time.time()
 
     fenetre.fill(0)
-    fenetre.blit(barre.image ,(barre.x ,barre.y))
+    fenetre.blit(barre.image,(barre.x ,barre.y))
+    fenetre.blit(ball.image,(ball.x,ball.y))
     pygame.display.flip()
 
 pygame.quit()
